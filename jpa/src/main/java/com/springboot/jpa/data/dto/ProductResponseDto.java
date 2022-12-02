7c1f8e0a3eacb58b5979663b2d2735761e87eef0
0901jbh <0901jbh@naver.com>
2022-12-02 19:49:49 +0900

    6 controller

---
+++ jpa/src/main/java/com/springboot/jpa/data/dto/ProductResponseDto.java
@@ -0,0 +1,14 @@
+package com.springboot.jpa.data.dto;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ProductResponseDto {
+    private Long number;
+    private String name;
+    private int price;
+    private int stock;
+}
